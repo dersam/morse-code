40c737f8fe5be6fad453423fda9812f67eb1f359
Sam Schmidt <samuel@dersam.net>
2022-06-02 20:15:02 -0400

    I care too much about whitespace

--- Morse.ino
+++ Morse.ino
@@ -1,10 +1,7 @@
-
 #include <LiquidCrystal.h>
 #define BUTTON_ON 1
 #define BUTTON_OFF 0
 
-// initialize the library by associating any needed LCD interface pin
-// with the arduino pin number it is connected to
 const int pushButton = 6;
 const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
@@ -15,7 +12,6 @@ int previousButtonState;
 long onStateBegan = 0;
 
 void setup() {
-  // set up the LCD's number of columns and rows:
   lcd.begin(16, 2);
   pinMode(pushButton, INPUT);
   previousButtonState = digitalRead(pushButton);
